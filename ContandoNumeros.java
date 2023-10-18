73603f577a9684888bff67ce85607bebc8594d34
rlopgar <rlopgar599@g.educaand.es>
2023-10-18 09:59:07 +0200

    Primer ejercicio

---
+++ ContandoNumeros.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ContandoNumeros {
+    public static void main(String[] args) {
+
+        int numeroNegativos=0;
+        int numeroPositivos=0;
+        int numeroCero=0;
+
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("Introduce 10 numeros:");
+
+        for (int i=1;i<=10;i++){
+            System.out.println("Numero "+i);
+            int numero= scan.nextInt();
+
+            if(numero>0){
+                numeroPositivos++;
+            }
+            else if(numero<0){
+                numeroNegativos++;
+            }
+            else{
+                numeroCero++;
+            }
+        }
+
+        System.out.println("Resultado:\nNumeros positivos: "+numeroPositivos+"\nNumeros negativos: "+numeroNegativos+"\nNumeros 0: "+numeroCero);
+
+
+    }
+}
