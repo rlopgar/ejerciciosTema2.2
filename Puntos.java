68bb63c77cc17535fa627417ab355a861b815595
rlopgar <rlopgar599@g.educaand.es>
2023-10-20 13:00:00 +0200

    Primer ejercicio

---
+++ Puntos.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Puntos {
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print(">");
+        String cadena=scan.nextLine();
+
+        System.out.print(">");
+        int longitud=scan.nextInt();
+
+        System.out.print(">");
+        char direccion=scan.next().charAt(0);
+
+        if (cadena.length()>longitud){
+
+        }
+    }
+}
